08fdec1786c7f244145e653bb564af1056d2ad5e
Angela <liny0207@gmail.com>
2018-10-16 20:45:36 +0000

    tests product detail features

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "Click the details and navigate to product page" do
+    # ACT
+    visit root_path
+
+    #CLICK
+
+    first("article.product").find_link("Details").click
+
+    # # DEBUG
+    # sleep 3
+    # save_screenshot
+  end
+end
